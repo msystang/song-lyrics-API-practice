6b1263b092a44633e2ff467cbeded709171c5a26
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-09 15:44:10 -0400

    initial commit

---
+++ song-lyrics-API-practice/song-lyrics-API-practiceTests/song_lyrics_API_practiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  song_lyrics_API_practiceTests.swift
+//  song-lyrics-API-practiceTests
+//
+//  Created by Sunni Tang on 9/9/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import XCTest
+@testable import song_lyrics_API_practice
+
+class song_lyrics_API_practiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
